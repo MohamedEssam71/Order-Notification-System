c2aa20f10402ed96f5a64336288331506019f6a3
Salah2Eddin <50306707+Salah2Eddin@users.noreply.github.com>
2023-12-26 12:38:10 +0200

    Made SendCommand Concrete
    Added Command interface
    Made SendCommand Concrete
    Removed SendEmailCommand and SendSMSCommand classes

--- OrderNotificationSystem/src/main/java/com/example/demo/model/Notifications/SendCommand.java
+++ OrderNotificationSystem/src/main/java/com/example/demo/model/Notifications/SendCommand.java
@@ -1,15 +1,27 @@
 package com.example.demo.model.Notifications;
 
-public abstract class SendCommand {
+public class SendCommand implements Command {
     String receiver;
+
     String message;
     SendService svc;
 
     public SendCommand(String receiver, String message, SendService svc) {
         this.receiver = receiver;
-        this.message=message;
+        this.message = message;
         this.svc = svc;
     }
 
-    public abstract void execute();
+    @Override
+    public void execute() {
+        svc.send(receiver, message);
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
