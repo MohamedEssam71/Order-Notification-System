6bc3f7a95719c775589a0abc1143d5f209887e28
Salah2Eddin <50306707+Salah2Eddin@users.noreply.github.com>
2023-12-29 20:42:04 +0200

    Added accounts to database

--- OrderNotificationSystem/src/main/java/com/example/demo/Database.java
+++ OrderNotificationSystem/src/main/java/com/example/demo/Database.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.model.Account.Account;
 import com.example.demo.model.Notifications.Notification;
 import com.example.demo.model.Order;
 
@@ -7,6 +8,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Database {
-    public static HashMap<String, Order> orderDB = new HashMap<String,Order>();
+    public static HashMap<String, Order> orderDB = new HashMap<>();
+    public static HashMap<String, Account> accounts = new HashMap<>();
     public static LinkedList<Notification> notificationsQueue = new LinkedList<>();
 }
