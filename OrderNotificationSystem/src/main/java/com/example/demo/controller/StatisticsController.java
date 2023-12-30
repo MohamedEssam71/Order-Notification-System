36d741adce3728f1f54afc26d743d04071055998
Salah2Eddin <50306707+Salah2Eddin@users.noreply.github.com>
2023-12-30 14:23:26 +0200

    Fixed Typo

--- OrderNotificationSystem/src/main/java/com/example/demo/controller/StatisticsController.java
+++ OrderNotificationSystem/src/main/java/com/example/demo/controller/StatisticsController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 
 import com.example.demo.service.IStatisticsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,17 +13,17 @@ public class StatisticsController {
     @Autowired
     IStatisticsService statisticsService;
 
-    @RequestMapping("/get/mostNotifiedEmail")
+    @GetMapping("/get/mostNotifiedEmail")
     public String getMostNotifiedEmail() {
         return statisticsService.getMostNotifiedEmail();
     }
 
-    @RequestMapping("/get/mostNotifiedSMS")
+    @GetMapping("/get/mostNotifiedSMS")
     public String getMostNotifiedSMS() {
         return statisticsService.getMostNotifiedSMS();
     }
 
-    @RequestMapping("/get/mostUsedTemplate")
+    @GetMapping("/get/mostUsedTemplate")
     public String getMostUsedTemplate() {
         return statisticsService.getMostUsedTemplate();
     }
