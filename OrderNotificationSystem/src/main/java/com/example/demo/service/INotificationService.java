1df5c93fee87b29fcbda48c7ed60fb323ceee4c9
Salah2Eddin <50306707+Salah2Eddin@users.noreply.github.com>
2023-12-30 15:01:35 +0200

    Notfication service creates notifications from given channel

--- OrderNotificationSystem/src/main/java/com/example/demo/service/INotificationService.java
+++ OrderNotificationSystem/src/main/java/com/example/demo/service/INotificationService.java
@@ -2,12 +2,13 @@ package com.example.demo.service;
 
 import com.example.demo.model.Account.Account;
 import com.example.demo.model.Notifications.Notification;
+import com.example.demo.model.Notifications.NotificationChannel;
 import com.fasterxml.jackson.databind.JsonNode;
 
 public interface INotificationService {
     public Notification createNotification(String username, String message, String type);
 
-    public boolean addNotification(Notification notification);
-    public boolean sendNotification();
+    public Boolean addNotification(NotificationChannel notificationChannel);
+    public Boolean sendNotification();
 
 }
